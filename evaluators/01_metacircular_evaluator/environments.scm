9676cb28ecab1fa1281625ffe1eb13e5b24fb188
asnr <asnr@users.noreply.github.com>
2019-08-05 08:50:29 +0530

    Add global environment, which programs can only read

--- evaluators/01_metacircular_evaluator/environments.scm
+++ evaluators/01_metacircular_evaluator/environments.scm
@@ -1,4 +1,40 @@
-(define (setup-environment)
-  the-empty-environment)
-
 (define the-empty-environment '())
+
+(define (setup-environment)
+  (let ((initial-env (extend-environment '() '() the-empty-environment)))
+    (define-variable! 'true true initial-env)
+    (define-variable! 'false false initial-env)
+    initial-env))
+
+(define (extend-environment vars vals base-env)
+  (let ((new-frame (cons vars vals)))
+    (cons new-frame base-env)))
+
+(define (lookup-variable-value variable-symbol env)
+
+  (define (lookup-value-in-frame names values)
+    (if (null? names)
+        (error "Variable name not found" variable-symbol)
+        (let ((curr-name (car names))
+              (curr-value (car values)))
+          (if (eq? curr-name variable-symbol)
+              curr-value
+              (lookup-value-in-frame (cdr names) (cdr values))))
+        )
+    )
+
+  (let* ((frame (car env))
+         (names (car frame))
+         (values (cdr frame)))
+    (lookup-value-in-frame names values))
+  )
+
+;; Redefining the same variable will shadow the original entry, not modify it
+(define (define-variable! var val env)
+  (let* ((frame (car env))
+         (names (car frame))
+         (values (cdr frame))
+         (new-names (cons var names))
+         (new-values (cons val values)))
+    (set-car! frame new-names)
+    (set-cdr! frame new-values)))
--- evaluators/01_metacircular_evaluator/eval.scm
+++ evaluators/01_metacircular_evaluator/eval.scm
@@ -1,7 +1,20 @@
 (define (metacircular-eval exp env)
-  (cond ((self-evaluating? exp) exp)))
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+        (else
+         (error "Unknown expression type: EVAL" exp))))
 
 (define (self-evaluating? exp)
   (cond ((number? exp) true)
         ((string? exp) true)
         (else false)))
+
+(define (variable? exp)
+  (symbol? exp))
+
+(define (quoted? exp)
+  (eq? (car exp) 'quote))
+
+(define (text-of-quotation exp)
+  (cadr exp))
